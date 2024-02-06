070cca7f6e3bd0ece33ee48e152f1ff1dde952e8
Maksym Charuk <maxcharuk@gmail.com>
2024-02-06 17:04:45 -0500

    Clean up GA code

--- src/hooks/useAnalyticsEventTracker.ts
+++ src/hooks/useAnalyticsEventTracker.ts
@@ -3,11 +3,6 @@ import ReactGA from "react-ga4";
 const useAnalyticsEventTracker = (category = "Default category") => {
   const eventTracker = (action = "Default action", label = "Default label") => {
     ReactGA.event({ category, action, label });
-    ReactGA.event({
-      category,
-      action,
-      label,
-    });
   };
   return eventTracker;
 };
